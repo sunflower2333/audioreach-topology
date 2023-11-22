sunflower2333/audioreach-topology

commit 81a74f825f671a64493e55236e022d5aed8730b1

audioreach: add support for LPASS v11.0.0
LPASS v11.0.0 breaks backward compatibility and legacy container
capability IDs are not working anymore.  Add a trailing argument to
stream-subgraph.m4 with LPASS compatibility version.  If v11.0.0 is
requested, use new defines for container capability ID.

Signed-off-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>
Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

audioreach/subgraph-stream-vol-playback.m4
@@ -30,7 +30,7 @@ undefine(`CONT_STACK_SIZE') dnl
 define(`PCM_PERF_MODE', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
 define(`PCM_DIRECTION', APM_SUB_GRAPH_DIRECTION_RX) dnl'
 define(`CONT_SENARIO_ID', APM_SUB_GRAPH_SID_AUDIO_PLAYBACK) dnl'
-define(`CONT_CAP', APM_CONTAINER_CAP_ID_CD) dnl'
+define(`CONT_CAP', `ifelse(LPASS_VER_COMPAT, 110000, APM_CONTAINER_TYPE_ID_GC, APM_CONTAINER_CAP_ID_CD)') dnl'
 define(`CONT_POSITION', APM_CONT_GRAPH_POS_STREAM) dnl'
 define(`PCM_DOMAIN_ID', APM_PROC_DOMAIN_ID_ADSP) dnl'
 define(`CONT_STACK_SIZE', 8192) dnl'

audioreach/audioreach.m4
@@ -7,14 +7,22 @@ define(`APM_SUB_GRAPH_DIRECTION_RX', `0x2') dnl
 define(`APM_SUB_GRAPH_SID_AUDIO_PLAYBACK', `0x1') dnl
 define(`APM_SUB_GRAPH_SID_AUDIO_RECORD', `0x2') dnl
 define(`APM_SUB_GRAPH_SID_VOICE_CALL', `0x3') dnl
-dnl container capability ID Pre/Post Processing (PP) 
+dnl container capability ID Pre/Post Processing (PP) (legacy)
 define(`APM_CONTAINER_CAP_ID_PP', `0x1') dnl
-dnl container capability ID Compression/Decompression (CD) 
+dnl container capability ID Compression/Decompression (CD) (legacy)
 define(`APM_CONTAINER_CAP_ID_CD', `0x2') dnl
-dnl container capability ID End Point(EP) 
+dnl container capability ID End Point(EP) (legacy)
 define(`APM_CONTAINER_CAP_ID_EP', `0x3') dnl
-dnl container capability ID Offload (OLC) 
+dnl container capability ID Offload (OLC) (legacy)
 define(`APM_CONTAINER_CAP_ID_OLC', `0x4') dnl
+dnl Container type identifier for a specialized container
+define(`APM_CONTAINER_TYPE_ID_SC', `0x0B001000') dnl
+dnl Container type identifier for a generic container
+define(`APM_CONTAINER_TYPE_ID_GC', `0x0B001001') dnl
+dnl Container type identifier for an offload container
+define(`APM_CONTAINER_TYPE_ID_OLC', `0x0B001002') dnl
+dnl Container type identifier for a wear container
+define(`APM_CONTAINER_TYPE_ID_WC', `0x0B001003') dnl
 define(`APM_CONT_GRAPH_POS_STREAM', `0x1') dnl
 dnl container graph position Per Stream Per Device
 define(`APM_CONT_GRAPH_POS_PER_STR_PER_DEV', `0x2') dnl

audioreach/stream-subgraph.m4
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 dnl STREAM_SG_PCM_ADD(stream, stream-dai-id,  
 dnl 	format, min-rate, max-rate, min-channels, max-hcannels,
-dnl	sg-iid-start, cont-iid-start, mod-iid-start)
+dnl	sg-iid-start, cont-iid-start, mod-iid-start, lpass-ver-compat)
 define(`STREAM_SG_PCM_ADD',
 `undefine(`PCM_DAI_ID') dnl'
 `undefine(`PCM_FORMAT') dnl'
@@ -23,5 +23,6 @@ define(`STREAM_SG_PCM_ADD',
 `define(`CONT_IID_START', `$9') dnl'
 `define(`MOD_IID_START', `$10') dnl'
 `define(`NAME_PREFIX', `stream$2') dnl'
+`define(`LPASS_VER_COMPAT', `$11') dnl'
 `include($1)') dnl
 

audioreach/subgraph-device-codec-dma-capture.m4
@@ -30,7 +30,7 @@ undefine(`CONT_STACK_SIZE') dnl
 define(`DEVICE_PERF_MODE', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
 define(`DEVICE_DIRECTION', APM_SUB_GRAPH_DIRECTION_TX) dnl'
 define(`CONT_SENARIO_ID', APM_SUB_GRAPH_SID_AUDIO_RECORD) dnl'
-define(`CONT_CAP', APM_CONTAINER_CAP_ID_EP) dnl'
+define(`CONT_CAP', `ifelse(LPASS_VER_COMPAT, 110000, APM_CONTAINER_TYPE_ID_GC, APM_CONTAINER_CAP_ID_EP)') dnl'
 define(`CONT_POSITION', APM_CONT_GRAPH_POS_GLOBAL_DEV)dnl'
 define(`DEVICE_DOMAIN_ID', APM_PROC_DOMAIN_ID_ADSP) dnl'
 define(`CONT_STACK_SIZE', 8192) dnl'

audioreach/subgraph-device-codec-dma-playback.m4
@@ -32,7 +32,7 @@ undefine(`CONT_STACK_SIZE') dnl
 define(`DEVICE_PERF_MODE', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
 define(`DEVICE_DIRECTION', APM_SUB_GRAPH_DIRECTION_TX) dnl'
 define(`CONT_SENARIO_ID', APM_SUB_GRAPH_SID_AUDIO_PLAYBACK) dnl'
-define(`CONT_CAP', APM_CONTAINER_CAP_ID_EP) dnl'
+define(`CONT_CAP', `ifelse(LPASS_VER_COMPAT, 110000, APM_CONTAINER_TYPE_ID_GC, APM_CONTAINER_CAP_ID_EP)') dnl'
 define(`CONT_POSITION', APM_CONT_GRAPH_POS_STREAM)dnl'
 define(`DEVICE_DOMAIN_ID', APM_PROC_DOMAIN_ID_ADSP) dnl'
 define(`CONT_STACK_SIZE', 8192) dnl'

audioreach/subgraph-device-i2s-playback.m4
@@ -32,7 +32,7 @@ undefine(`CONT_STACK_SIZE') dnl
 define(`DEVICE_PERF_MODE', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
 define(`DEVICE_DIRECTION', APM_SUB_GRAPH_DIRECTION_TX) dnl'
 define(`CONT_SENARIO_ID', APM_SUB_GRAPH_SID_AUDIO_PLAYBACK) dnl'
-define(`CONT_CAP', APM_CONTAINER_CAP_ID_EP) dnl'
+define(`CONT_CAP', `ifelse(LPASS_VER_COMPAT, 110000, APM_CONTAINER_TYPE_ID_GC, APM_CONTAINER_CAP_ID_EP)') dnl'
 define(`CONT_POSITION', APM_CONT_GRAPH_POS_STREAM)dnl'
 define(`DEVICE_DOMAIN_ID', APM_PROC_DOMAIN_ID_ADSP) dnl'
 define(`CONT_STACK_SIZE', 8192) dnl'

audioreach/subgraph-stream-capture.m4
@@ -30,7 +30,7 @@ undefine(`CONT_STACK_SIZE') dnl
 define(`PCM_PERF_MODE', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
 define(`PCM_DIRECTION', APM_SUB_GRAPH_DIRECTION_TX) dnl'
 define(`CONT_SENARIO_ID', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
-define(`CONT_CAP', APM_CONTAINER_CAP_ID_CD) dnl'
+define(`CONT_CAP', `ifelse(LPASS_VER_COMPAT, 110000, APM_CONTAINER_TYPE_ID_GC, APM_CONTAINER_CAP_ID_CD)') dnl'
 define(`CONT_POSITION', APM_CONT_GRAPH_POS_GLOBAL_DEV) dnl'
 define(`PCM_DOMAIN_ID', APM_PROC_DOMAIN_ID_ADSP) dnl'
 define(`CONT_STACK_SIZE', 8192) dnl'

audioreach/subgraph-stream-playback.m4
@@ -28,7 +28,7 @@ undefine(`CONT_STACK_SIZE') dnl
 define(`PCM_PERF_MODE', APM_SG_PERF_MODE_LOW_LATENCY) dnl'
 define(`PCM_DIRECTION', APM_SUB_GRAPH_DIRECTION_RX) dnl'
 define(`CONT_SENARIO_ID', APM_SUB_GRAPH_SID_AUDIO_PLAYBACK) dnl'
-define(`CONT_CAP', APM_CONTAINER_CAP_ID_CD) dnl'
+define(`CONT_CAP', `ifelse(LPASS_VER_COMPAT, 110000, APM_CONTAINER_TYPE_ID_GC, APM_CONTAINER_CAP_ID_CD)') dnl'
 define(`CONT_POSITION', APM_CONT_GRAPH_POS_STREAM) dnl'
 define(`PCM_DOMAIN_ID', APM_PROC_DOMAIN_ID_ADSP) dnl'
 define(`CONT_STACK_SIZE', 8192) dnl'

commit 35acc13112c3f2aeb92a437aa81cde6dece17113

audioreach: stream-vol-playback: fix the container id for new version
Container Id is not correctly setup for new version of LPASS, fix this.

Fixes: 81a74f825f67 ("audioreach: add support for LPASS v11.0.0")
Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@linaro.org>

audioreach/subgraph-stream-vol-playback.m4
@@ -65,7 +65,7 @@ AR_PCM_PLAYBACK(PCM_DAI_ID, PCM_FORMAT, PCM_MIN_RATE, PCM_MAX_RATE, PCM_MIN_CHAN
 
 AR_SUBGRAPH(SG_INDEX, PCM_DAI_ID, SG_IID_START, PCM_PERF_MODE, PCM_DIRECTION, CONT_SENARIO_ID)
 
-AR_CONTAINER(CONTAINER_INDEX, CONT_IID_START,  APM_CONTAINER_CAP_ID_CD, 8192, APM_CONT_GRAPH_POS_STREAM, APM_PROC_DOMAIN_ID_ADSP)
+AR_CONTAINER(CONTAINER_INDEX, CONT_IID_START,  CONT_CAP, 8192, APM_CONT_GRAPH_POS_STREAM, APM_PROC_DOMAIN_ID_ADSP)
 
 AR_MODULE_WRSH(MOD_INDEX, SG_INDEX, CONTAINER_INDEX,	WRSH_MODULE_IID,  0, 1, 0, 0, 1, 2, PCM_DAI_ID, PCMDEC_MODULE_IID)
 AR_MODULE_PCMDEC(MOD_INDEX, SG_INDEX, CONTAINER_INDEX,	PCMDEC_MODULE_IID, 1, 1, 2, 1, 1, 2, PCM_INTERLEAVED,PCMCNV_MODULE_IID)